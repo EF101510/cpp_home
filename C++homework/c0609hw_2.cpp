9bd0f9092822ff7d62202f2fbb1709936953ce2e
hectorliu <hectorliukiki@gmail.com>
2025-07-18 20:07:11 +0800

    feat: update square function to use default parameter and enhance main function with variable initialization

--- C++homework/c0609hw_2.cpp
+++ C++homework/c0609hw_2.cpp
@@ -5,22 +5,30 @@ using namespace std;
 int square(int);
 void swap(int& ,int&);
 
+int a = 0;
+
+
 int main(int argc, char** argv) 
 {
+    // extern int a;
     int cin_square = 2;
     int cin_swap = 5;
     int cin_swap2 = 3;
+    auto x = 0.0;
+
     int cout_square = square(cin_square);
     cout << cout_square << endl;
 
     cout << "交換前：cin_swap:" << cin_swap << "cin_swap2:" << cin_swap2 <<endl;
     swap(cin_swap,cin_swap2);
     cout << "交換後：cin_swap:" << cin_swap << "cin_swap2:" << cin_swap2 <<endl;
+    a = 6;
+
 
     return 0;
 }
 
-int square(int x)
+int square(int x = 5)
 {
     return x*x;
 }
@@ -32,3 +40,4 @@ void swap (int& y ,int& z)
     y = z;
     z = temp;
 }
+
