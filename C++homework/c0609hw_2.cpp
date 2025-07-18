ea7c7f4c4f922647c97c4f6b3d684a08906cec3b
hectorliu <hectorliukiki@gmail.com>
2025-07-18 19:36:16 +0800

    feat: add C++ program for square calculation and swapping values

---
+++ C++homework/c0609hw_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// 函數宣告
+int square(int);
+void swap(int& ,int&);
+
+int main(int argc, char** argv) 
+{
+    int cin_square = 2;
+    int cin_swap = 5;
+    int cin_swap2 = 3;
+    int cout_square = square(cin_square);
+    cout << cout_square << endl;
+
+    cout << "交換前：cin_swap:" << cin_swap << "cin_swap2:" << cin_swap2 <<endl;
+    swap(cin_swap,cin_swap2);
+    cout << "交換後：cin_swap:" << cin_swap << "cin_swap2:" << cin_swap2 <<endl;
+
+    return 0;
+}
+
+int square(int x)
+{
+    return x*x;
+}
+
+void swap (int& y ,int& z)
+{
+    int temp;
+    temp = y;
+    y = z;
+    z = temp;
+}
