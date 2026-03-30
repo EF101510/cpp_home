f81014870adc92f41d770086b5f6f0a1b975462f
EF101510 <hectorliukiki@gmail.com>
2026-03-30 19:25:50 +0800

    feat: add C++ program about Jacob

---
+++ C++homework/Jacob.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Jacob is gay? YES = 1 or NO = 2" << endl;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << "You are right." << endl;
+    }
+    if (n == 2)
+    {
+        cout << "You're wrong. Jacob is gay." << endl;
+    }
+}
